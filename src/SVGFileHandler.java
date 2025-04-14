d35908a433accd228947b44c3be45e7b6602d379
Altancho04 <altan04@abv.bg>
2025-04-14 20:57:53 +0300

    .

---
+++ src/SVGFileHandler.java
@@ -0,0 +1,39 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class SVGFileHandler {
+    public void openFile(String filename, ShapeManager shapeManager) throws IOException {
+        Path path = Paths.get(filename);
+        List<String> lines = Files.readAllLines(path);
+
+        for (String line : lines) {
+            if (line.contains("<line")) {
+                int x1 = Integer.parseInt(line.split("x1=\"")[1].split("\"")[0]);
+                int y1 = Integer.parseInt(line.split("y1=\"")[1].split("\"")[0]);
+                int x2 = Integer.parseInt(line.split("x2=\"")[1].split("\"")[0]);
+                int y2 = Integer.parseInt(line.split("y2=\"")[1].split("\"")[0]);
+                shapeManager.addShape(new Line(x1, y1, x2, y2));
+            } else if (line.contains("<circle")) {
+                double cx = Double.parseDouble(line.split("cx=\"")[1].split("\"")[0]);
+                double cy = Double.parseDouble(line.split("cy=\"")[1].split("\"")[0]);
+                double r = Double.parseDouble(line.split("r=\"")[1].split("\"")[0]);
+                shapeManager.addShape(new Circle(cx, cy, r, "black"));
+            } else if (line.contains("<rect")) {
+                double x = Double.parseDouble(line.split("x=\"")[1].split("\"")[0]);
+                double y = Double.parseDouble(line.split("y=\"")[1].split("\"")[0]);
+                double width = Double.parseDouble(line.split("width=\"")[1].split("\"")[0]);
+                double height = Double.parseDouble(line.split("height=\"")[1].split("\"")[0]);
+                shapeManager.addShape(new Rectangle(x, y, width, height, "black"));
+            }
+        }
+    }
+
+    public void saveFile(String filename, ShapeManager shapeManager) throws IOException {
+        Path path = Paths.get(filename);
+        String svgContent = shapeManager.toSVG();
+        Files.write(path, svgContent.getBytes());
+    }
+}
---
+++ src/ShapeManager.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShapeManager {
+    private List<Shape> shapes = new ArrayList<>();
+
+    public void addShape(Shape shape) {
+        shapes.add(shape);
+    }
+
+    public void removeShape(Shape shape) {
+        shapes.remove(shape);
+    }
+
+    public List<Shape> getShapes() {
+        return shapes;
+    }
+
+    public void moveShape(Shape shape, int deltaX, int deltaY) {
+        shape.move(deltaX, deltaY);
+    }
+
+    public String toSVG() {
+        StringBuilder svg = new StringBuilder("<svg xmlns=\"http://www.w3.org/2000/svg\">\n");
+        for (Shape shape : shapes) {
+            svg.append(shape.toSVG()).append("\n");
+        }
+        svg.append("</svg>");
+        return svg.toString();
+    }
+}
